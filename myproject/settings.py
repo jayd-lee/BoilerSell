fc1185910ecadcbf8ab9c2af8f6c7db78e63e0bb
Jayden Lee <jaydeh.lee@gmail.com>
2023-04-28 03:41:08 -0400

    postgres

--- myproject/settings.py
+++ myproject/settings.py
@@ -84,12 +84,24 @@ ASGI_APPLICATION = 'myproject.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boilerselldb',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'jayden94qbw3',
+        'HOST': 'boilerselldb.c9zozrg8m8zj.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
