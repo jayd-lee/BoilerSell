2828376ee06d76d6dcf2446ea4981628f5a7d6b2
Jayden Lee <jaydeh.lee@gmail.com>
2023-04-28 13:36:54 -0400

    hi

--- myproject/settings.py
+++ myproject/settings.py
@@ -155,8 +155,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
